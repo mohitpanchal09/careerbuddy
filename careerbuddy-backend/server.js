7e90eb9f83bb7e600fd744d0cf4cdfcf917a2c24
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-08-25 11:44:53 +0530

    server

--- careerbuddy-backend/server.js
+++ careerbuddy-backend/server.js
@@ -2,13 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import mongoose from 'mongoose'
 import authRouter from './routers/authRouter.js'
 import userRouter from './routers/userRouter.js'
 
 const app = express()
 
 dotenv.config({path: './config/config.env'})
+
 connectDB()
+
 
 app.use(express.json())
 app.use(cors())
